ea3fea25e22d550dc6858ba74a876593437dc752
monniiaa <sofielarsen2411@gmail.com>
2025-10-20 10:48:17 +0200

    s

--- self-localization/selflocalize.py
+++ self-localization/selflocalize.py
@@ -43,7 +43,7 @@ CBLACK = (0, 0, 0)
 
 # Landmarks.
 # The robot knows the position of 2 landmarks. Their coordinates are in the unit centimeters [cm].
-landmarkIDs = [6, 7]
+landmarkIDs = [7, 6]
 landmarks = {
     6: (0.0, 0.0),  # Coordinates for landmark 1
     7: (300.0, 0.0)  # Coordinates for landmark 2
