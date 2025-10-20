05e7377d152bfeab3a541e0736230457b33dce41
monniiaa <sofielarsen2411@gmail.com>
2025-10-20 14:00:08 +0200

    s

--- self-localization/selflocalize.py
+++ self-localization/selflocalize.py
@@ -45,8 +45,8 @@ CBLACK = (0, 0, 0)
 # The robot knows the position of 2 landmarks. Their coordinates are in the unit centimeters [cm].
 landmarkIDs = [6, 7]
 landmarks = {
-    6: (300.0, 0.0),  # Coordinates for landmark 1
-    7: (0.0, 0.0)  # Coordinates for landmark 2
+    6: (0.0, 0.0),  # Coordinates for landmark 1
+    7: (300.0, 0.0)  # Coordinates for landmark 2
 }
 
 center = np.array([(landmarks[6][0] + landmarks[7][0]) / 2,
@@ -204,8 +204,8 @@ try:
     distance = 0.0 # distance driven at this time step
     angle = 0.0 # angle turned at this timestep
 
-    sigma_d = 10
-    sigma_theta = 0.3
+    sigma_d = 15
+    sigma_theta = 0.03
     counter = 0
     #Initialize the robot
     if isRunningOnArlo():
